962d2dd72ce4042758d3cc6321b70131fd8666d4
sasuke_urahara <128579274+sasukeurahara@users.noreply.github.com>
2025-04-18 21:45:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def intin():
+    return (int(input()))
+def lin():
+    return (list(map(int,input().split())))
+def mint():
+    return (map(int,input().split()))
+def frequency(a):
+    d = {x:a.count(x) for x in a}
+    return d    
+mod = int(1e9+7)
+
+def solve(n):
+    
+    return 
+
+for _ in range(intin()):
+
+
+    print()
